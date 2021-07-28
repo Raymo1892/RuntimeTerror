32b74f2a58750dc2c605daf3b08babbc7e6e9165
youngjaytea <deimosjt@gmail.com>
2021-07-28 05:43:50 -0500

    I updated the Inventory GUI. Work in progress

---
+++ build-Gas_Station_Project-Desktop_Qt_5_15_2_MinGW_64_bit-Debug/debug/moc_homepagewindow.cpp
@@ -0,0 +1,274 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'homepagewindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../Gas_Station_Project/homepagewindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'homepagewindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_HomepageWindow_t {
+    QByteArrayData data[36];
+    char stringdata0[915];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_HomepageWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_HomepageWindow_t qt_meta_stringdata_HomepageWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 14), // "HomepageWindow"
+QT_MOC_LITERAL(1, 15, 21), // "on_homeButton_clicked"
+QT_MOC_LITERAL(2, 37, 0), // ""
+QT_MOC_LITERAL(3, 38, 24), // "on_accountButton_clicked"
+QT_MOC_LITERAL(4, 63, 23), // "on_logOutButton_clicked"
+QT_MOC_LITERAL(5, 87, 26), // "on_inventoryButton_clicked"
+QT_MOC_LITERAL(6, 114, 22), // "on_auditButton_clicked"
+QT_MOC_LITERAL(7, 137, 24), // "on_settingButton_clicked"
+QT_MOC_LITERAL(8, 162, 36), // "on_returnFromInventoryButton_..."
+QT_MOC_LITERAL(9, 199, 32), // "on_returnFromAuditButton_clicked"
+QT_MOC_LITERAL(10, 232, 23), // "on_pushButton_3_clicked"
+QT_MOC_LITERAL(11, 256, 21), // "on_helpButton_clicked"
+QT_MOC_LITERAL(12, 278, 25), // "on_unleadedButton_clicked"
+QT_MOC_LITERAL(13, 304, 24), // "on_premiumButton_clicked"
+QT_MOC_LITERAL(14, 329, 23), // "on_dieselButton_clicked"
+QT_MOC_LITERAL(15, 353, 26), // "on_grillItemButton_clicked"
+QT_MOC_LITERAL(16, 380, 22), // "on_candyButton_clicked"
+QT_MOC_LITERAL(17, 403, 22), // "on_chipsButton_clicked"
+QT_MOC_LITERAL(18, 426, 23), // "on_coffeeButton_clicked"
+QT_MOC_LITERAL(19, 450, 30), // "on_fountainDrinkButton_clicked"
+QT_MOC_LITERAL(20, 481, 27), // "on_bottledBevButton_clicked"
+QT_MOC_LITERAL(21, 509, 26), // "on_viewAuditButton_clicked"
+QT_MOC_LITERAL(22, 536, 44), // "on_pushButton_ChangeEmployeeP..."
+QT_MOC_LITERAL(23, 581, 26), // "on_clearCartButton_clicked"
+QT_MOC_LITERAL(24, 608, 31), // "on_ChangePasswordButton_clicked"
+QT_MOC_LITERAL(25, 640, 24), // "on_addUserButton_clicked"
+QT_MOC_LITERAL(26, 665, 27), // "on_removeUserButton_clicked"
+QT_MOC_LITERAL(27, 693, 31), // "on_returnAccountsButton_clicked"
+QT_MOC_LITERAL(28, 725, 32), // "on_returnAccountsButton1_clicked"
+QT_MOC_LITERAL(29, 758, 32), // "on_returnAccountsButton2_clicked"
+QT_MOC_LITERAL(30, 791, 25), // "on_saveUserButton_clicked"
+QT_MOC_LITERAL(31, 817, 27), // "on_deleteUserButton_clicked"
+QT_MOC_LITERAL(32, 845, 18), // "changeCurrentIndex"
+QT_MOC_LITERAL(33, 864, 5), // "index"
+QT_MOC_LITERAL(34, 870, 23), // "on_InventoryQty_clicked"
+QT_MOC_LITERAL(35, 894, 20) // "on_Tableview_clicked"
+
+    },
+    "HomepageWindow\0on_homeButton_clicked\0"
+    "\0on_accountButton_clicked\0"
+    "on_logOutButton_clicked\0"
+    "on_inventoryButton_clicked\0"
+    "on_auditButton_clicked\0on_settingButton_clicked\0"
+    "on_returnFromInventoryButton_clicked\0"
+    "on_returnFromAuditButton_clicked\0"
+    "on_pushButton_3_clicked\0on_helpButton_clicked\0"
+    "on_unleadedButton_clicked\0"
+    "on_premiumButton_clicked\0"
+    "on_dieselButton_clicked\0"
+    "on_grillItemButton_clicked\0"
+    "on_candyButton_clicked\0on_chipsButton_clicked\0"
+    "on_coffeeButton_clicked\0"
+    "on_fountainDrinkButton_clicked\0"
+    "on_bottledBevButton_clicked\0"
+    "on_viewAuditButton_clicked\0"
+    "on_pushButton_ChangeEmployeePassword_clicked\0"
+    "on_clearCartButton_clicked\0"
+    "on_ChangePasswordButton_clicked\0"
+    "on_addUserButton_clicked\0"
+    "on_removeUserButton_clicked\0"
+    "on_returnAccountsButton_clicked\0"
+    "on_returnAccountsButton1_clicked\0"
+    "on_returnAccountsButton2_clicked\0"
+    "on_saveUserButton_clicked\0"
+    "on_deleteUserButton_clicked\0"
+    "changeCurrentIndex\0index\0"
+    "on_InventoryQty_clicked\0on_Tableview_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_HomepageWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      33,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  179,    2, 0x08 /* Private */,
+       3,    0,  180,    2, 0x08 /* Private */,
+       4,    0,  181,    2, 0x08 /* Private */,
+       5,    0,  182,    2, 0x08 /* Private */,
+       6,    0,  183,    2, 0x08 /* Private */,
+       7,    0,  184,    2, 0x08 /* Private */,
+       8,    0,  185,    2, 0x08 /* Private */,
+       9,    0,  186,    2, 0x08 /* Private */,
+      10,    0,  187,    2, 0x08 /* Private */,
+      11,    0,  188,    2, 0x08 /* Private */,
+      12,    0,  189,    2, 0x08 /* Private */,
+      13,    0,  190,    2, 0x08 /* Private */,
+      14,    0,  191,    2, 0x08 /* Private */,
+      15,    0,  192,    2, 0x08 /* Private */,
+      16,    0,  193,    2, 0x08 /* Private */,
+      17,    0,  194,    2, 0x08 /* Private */,
+      18,    0,  195,    2, 0x08 /* Private */,
+      19,    0,  196,    2, 0x08 /* Private */,
+      20,    0,  197,    2, 0x08 /* Private */,
+      21,    0,  198,    2, 0x08 /* Private */,
+      22,    0,  199,    2, 0x08 /* Private */,
+      23,    0,  200,    2, 0x08 /* Private */,
+      24,    0,  201,    2, 0x08 /* Private */,
+      25,    0,  202,    2, 0x08 /* Private */,
+      26,    0,  203,    2, 0x08 /* Private */,
+      27,    0,  204,    2, 0x08 /* Private */,
+      28,    0,  205,    2, 0x08 /* Private */,
+      29,    0,  206,    2, 0x08 /* Private */,
+      30,    0,  207,    2, 0x08 /* Private */,
+      31,    0,  208,    2, 0x08 /* Private */,
+      32,    1,  209,    2, 0x08 /* Private */,
+      34,    0,  212,    2, 0x08 /* Private */,
+      35,    0,  213,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   33,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void HomepageWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<HomepageWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_homeButton_clicked(); break;
+        case 1: _t->on_accountButton_clicked(); break;
+        case 2: _t->on_logOutButton_clicked(); break;
+        case 3: _t->on_inventoryButton_clicked(); break;
+        case 4: _t->on_auditButton_clicked(); break;
+        case 5: _t->on_settingButton_clicked(); break;
+        case 6: _t->on_returnFromInventoryButton_clicked(); break;
+        case 7: _t->on_returnFromAuditButton_clicked(); break;
+        case 8: _t->on_pushButton_3_clicked(); break;
+        case 9: _t->on_helpButton_clicked(); break;
+        case 10: _t->on_unleadedButton_clicked(); break;
+        case 11: _t->on_premiumButton_clicked(); break;
+        case 12: _t->on_dieselButton_clicked(); break;
+        case 13: _t->on_grillItemButton_clicked(); break;
+        case 14: _t->on_candyButton_clicked(); break;
+        case 15: _t->on_chipsButton_clicked(); break;
+        case 16: _t->on_coffeeButton_clicked(); break;
+        case 17: _t->on_fountainDrinkButton_clicked(); break;
+        case 18: _t->on_bottledBevButton_clicked(); break;
+        case 19: _t->on_viewAuditButton_clicked(); break;
+        case 20: _t->on_pushButton_ChangeEmployeePassword_clicked(); break;
+        case 21: _t->on_clearCartButton_clicked(); break;
+        case 22: _t->on_ChangePasswordButton_clicked(); break;
+        case 23: _t->on_addUserButton_clicked(); break;
+        case 24: _t->on_removeUserButton_clicked(); break;
+        case 25: _t->on_returnAccountsButton_clicked(); break;
+        case 26: _t->on_returnAccountsButton1_clicked(); break;
+        case 27: _t->on_returnAccountsButton2_clicked(); break;
+        case 28: _t->on_saveUserButton_clicked(); break;
+        case 29: _t->on_deleteUserButton_clicked(); break;
+        case 30: _t->changeCurrentIndex((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 31: _t->on_Tableview_clicked(); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject HomepageWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_HomepageWindow.data,
+    qt_meta_data_HomepageWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *HomepageWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *HomepageWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_HomepageWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int HomepageWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 33)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 33;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 33)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 33;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
