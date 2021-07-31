838066fdafa711ecd35e7a48e573d6421d7890ab
Michael Wade <36501855+bercilak13@users.noreply.github.com>
2021-07-31 10:51:09 -0500

    No more local inventory vector!

--- Gas_Station_Project/homepagewindow.cpp
+++ Gas_Station_Project/homepagewindow.cpp
@@ -15,23 +15,12 @@ QString cartString;
 QString totalString;
 double cartTotal = 0.0;
 mysql db;
-int idFromDB;
-QString nameFromDB;
-double priceFromDB;
-bool inStockFromDB;
-QString categoryFromDB;
-int idNumber;
-
-QVector<Items> itemsVector;
-Items item1(0, "", 0, false, "", 0);
-Items item2(0, "", 0, false, "", 0);
-Items item3(0, "", 0, false, "", 0);
-Items item4(0, "", 0, false, "", 0);
-Items item5(0, "", 0, false, "", 0);
-Items item6(0, "", 0, false, "", 0);
-Items item7(0, "", 0, false, "", 0);
-Items item8(0, "", 0, false, "", 0);
-Items item9(0, "", 0, false, "", 0);
+//int idFromDB;
+//QString nameFromDB;
+//double priceFromDB;
+//bool inStockFromDB;
+//QString categoryFromDB;
+//int idNumber;
 
 using namespace std;
 
@@ -40,49 +29,6 @@ HomepageWindow::HomepageWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::HomepageWindow)
 {
-    if(db.connectDB())
-    {
-        QSqlQuery qry;
-        //Read from database
-        for(idNumber = 1; idNumber < 100; idNumber++){
-            qry.prepare(QString("SELECT * FROM INVENTORY WHERE itemid = :idNumber"));
-            //qry.prepare(QString("SELECT * FROM USERS WHERE username = :username AND password = :password"));
-            //binding variable with values column
-            qry.bindValue(":idNumber", idNumber);
-
-            if(!qry.exec())
-            {
-                QMessageBox::warning(this, "Failed", "Query Failed to Execute ");
-
-            }
-            else
-            {
-                while(qry.next())
-                {
-                      idFromDB = qry.value(0).toInt();
-                      nameFromDB = qry.value(1).toString();
-                      priceFromDB = qry.value(2).toDouble();
-                      inStockFromDB = qry.value(3).toBool();
-                      categoryFromDB = qry.value(4).toString();
-                }
-                Items *newitem = new Items;
-                newitem->setID(idFromDB);
-                newitem->setName(nameFromDB);
-                newitem->setPrice(priceFromDB / 100);
-                newitem->setStock(inStockFromDB);
-                newitem->setCategory(categoryFromDB);
-                itemsVector.push_back(*newitem);
-
-                }
-            }
-
-        }
-        else
-        {
-            QMessageBox::information(this, "Not Connected", "Database is not Connected");
-        }
-        db.closeDB();
-
     ui->setupUi(this);
     ui->stackedWidget->setCurrentIndex(0);
 
@@ -358,25 +304,45 @@ void HomepageWindow::on_addToCartButton_clicked()
 {
     QString itemID = ui->makeSaleItemIDLineEdit->text();
     QString quantity = ui->makeSaleQuantityLineEdit->text();
-
-    //QMessageBox::information(this, "Add to Cart", "You entered: \n" + itemID + "\n" + quantity + "\n");
-
-
+    double itemPrice = 0;
+    QString itemName = "";
 
 
-    for(int i = 0; i < itemsVector.size(); i++)
+    if(db.connectDB())
     {
-        if(itemsVector[i].id == itemID.toInt())
-        {
-            double itemTotal = quantity.toInt() * itemsVector[i].price;
-            ui->cartDisplay->setText(ui->cartDisplay->text() + "\n" + quantity + "X " + itemsVector[i].name + " - " + QString::number(itemTotal));
-            cartTotal += itemTotal;
+        QSqlQuery qry;
+        //Read from database
+        qry.prepare(QString("SELECT * FROM INVENTORY WHERE itemid = :itemid"));
+        //binding variable with values column
+        qry.bindValue(":itemid", itemID);
 
-            totalString = QString::number(cartTotal); // this converts the cartTotal variable into a string to be displayed
-            ui->totalBoxDisplay->setText("$" + totalString);
+        if(!qry.exec())
+        {
+            QMessageBox::warning(this, "Failed", "Query Failed to Execute ");
+
+        }
+        else
+        {
+            while(qry.next())
+            {
+                itemName = qry.value(1).toString();
+                itemPrice = qry.value(2).toDouble() / 100;
+            }
+
         }
     }
+    else
+    {
+        QMessageBox::information(this, "Not Connected", "Database is not Connected");
+    }
+    db.closeDB();
 
+    double itemTotal = quantity.toDouble() * itemPrice;
+    ui->cartDisplay->setText(ui->cartDisplay->text() + "\n" + quantity + "X " + itemName + " - " + QString::number(itemTotal));
+    cartTotal += itemTotal;
+
+    totalString = QString::number(cartTotal); // this converts the cartTotal variable into a string to be displayed
+    ui->totalBoxDisplay->setText("$" + totalString);
 
 
 }
@@ -456,37 +422,21 @@ void HomepageWindow::on_addItemButtonInventory_clicked()
     QString category = ui->inventoryCategoryLineEdit->text();
     QString quantity = ui->inventoryQuantityLineEdit->text();
 
-    bool itemMatchFound = false;
-    int i = 0;
-
-    for(i = 0; i < itemsVector.size(); i++)
-    {
-        if(itemsVector[i].id == itemID.toInt())
-        {
-           QMessageBox::information(this, "Add Item", "ItemID already found in database, please try again.");
-           itemMatchFound = true;
-           break;
-        }
-    }
-
-    if(itemMatchFound != true)
-    {
-        Items tempObj(itemID.toInt(), itemName, price.toDouble(), true, category, quantity.toInt());
-        itemsVector.push_back(tempObj);if(db.connectDB())
+    if(db.connectDB())
         {
             //Write query to database
             QSqlQuery qry;
             qry.prepare("INSERT INTO INVENTORY VAlUES (:itemid, :itemname, :price, :instock, :quantity, :category)");
             //binding variable with values column
-            qry.bindValue(":itemid", tempObj.getID());
-            qry.bindValue(":itemname", tempObj.getName());
-            qry.bindValue(":price", tempObj.getPrice()*100);
-            if(tempObj.getQuantity() > 0)
+            qry.bindValue(":itemid", itemID.toInt());
+            qry.bindValue(":itemname", itemName);
+            qry.bindValue(":price", price.toDouble()*100);
+            if(quantity.toInt() > 0)
                 qry.bindValue(":instock", true);
             else
                 qry.bindValue(":instock", false);
-            qry.bindValue(":quantity", tempObj.getQuantity());
-            qry.bindValue(":category", tempObj.getCategory());
+            qry.bindValue(":quantity", quantity.toInt());
+            qry.bindValue(":category", category);
 
             if(!qry.exec())
             {
@@ -495,7 +445,7 @@ void HomepageWindow::on_addItemButtonInventory_clicked()
             }
             else
             {
-                QMessageBox::information(this, "Inserted", "Item added to Inventory");
+                QMessageBox::information(this, "Add Item", "Item added to Inventory");
 
             }
         }
@@ -504,58 +454,38 @@ void HomepageWindow::on_addItemButtonInventory_clicked()
             QMessageBox::information(this, "Not Connected", "Database is not Connected");
         }
 
-        //hide();
         db.closeDB();
-        QMessageBox::information(this, "Add Item", "Item successfully added.");
-    }
 }
+
 
 
 void HomepageWindow::on_removeItemButton_clicked()
 {
     QString itemID = ui->inventoryIDLineEdit->text();
 
-    bool itemMatchFound = false;
-
-
-    for(int i = 0; i < itemsVector.size(); i++)
+    if(db.connectDB())
     {
-        if(itemsVector[i].id == itemID.toInt())
-        {
-           itemMatchFound = true;
-           itemsVector.remove(i);
-           QMessageBox::information(this, "Remove Item", "Item successfully deleted.");
-           if(db.connectDB())
-           {
-               QSqlQuery qry;
-               qry.prepare("DELETE FROM INVENTORY WHERE itemid = :itemid");
-               //binding variable with values column
-               qry.bindValue(":itemid", itemID);
-               if(!qry.exec()){
-                   QMessageBox::warning(this, "Failed", "Delete Failed to Execute ");
-               }
-               else
-               {
-                   QMessageBox::information(this, "Deleted", "Item removed from Inventory");
-               }
-
-           }
-           else
-           {
-               QMessageBox::information(this, "Not Connected", "Database is not Connected");
-           }
-           db.closeDB();
-           break;
-        }
-    }
-
-    if(itemMatchFound != true)
-    {
-
-        QMessageBox::information(this, "Remove Item", "No matching Item ID found. Please try again.");
+       QSqlQuery qry;
+       qry.prepare("DELETE FROM INVENTORY WHERE itemid = :itemid");
+       //binding variable with values column
+       qry.bindValue(":itemid", itemID);
+       if(!qry.exec()){
+           QMessageBox::warning(this, "Failed", "Delete Failed to Execute ");
+       }
+       else
+       {
+           QMessageBox::information(this, "Deleted", "Item removed from Inventory");
+       }
 
     }
+    else
+    {
+       QMessageBox::information(this, "Not Connected", "Database is not Connected");
+    }
+    db.closeDB();
 }
+
+
 
 
 void HomepageWindow::on_updateInventoryButton_clicked()
@@ -566,61 +496,34 @@ void HomepageWindow::on_updateInventoryButton_clicked()
     QString category = ui->inventoryCategoryLineEdit->text();
     QString quantity = ui->inventoryQuantityLineEdit->text();
 
-    bool itemMatchFound = false;
-
-
-    for(int i = 0; i < itemsVector.size(); i++)
+    if(db.connectDB())
     {
-        if(itemsVector[i].id == itemID.toInt())
-        {
-           itemMatchFound = true;
-
-
-
+        QSqlQuery qry;
+        qry.prepare("UPDATE INVENTORY SET itemname = :itemname, price = :price, instock = :instock, quantity = :quantity, category = :category WHERE itemid = :itemid");
+        //binding variable with values column
+        qry.bindValue(":itemid", itemID.toInt());
+        qry.bindValue(":itemname", itemName);
+        qry.bindValue(":price", price.toDouble()*100);
+        qry.bindValue(":quantity", quantity.toInt());
+        qry.bindValue(":category", category);
+        if(quantity.toInt() > 0) qry.bindValue(":instock", true);
+        else qry.bindValue(":instock", false);
+        if(!qry.exec()){
+            QMessageBox::warning(this, "Failed", "Update Failed to Execute ");
         }
-        if(itemMatchFound == true)
+        else
         {
-            itemsVector[i].id = itemID.toInt();
-            itemsVector[i].name = itemName;
-            itemsVector[i].price = price.toDouble();
-            itemsVector[i].category = category;
-            itemsVector[i].quantity = quantity.toInt();
-            QMessageBox::information(this, "Update Item", "Item successfully updated.");
-            if(db.connectDB())
-            {
-                QSqlQuery qry;
-                qry.prepare("UPDATE INVENTORY SET itemname = :itemname, price = :price, instock = :instock, quantity = :quantity, category = :category WHERE itemid = :itemid");
-                //binding variable with values column
-                qry.bindValue(":itemid", itemsVector[i].id);
-                qry.bindValue(":itemname", itemsVector[i].name);
-                qry.bindValue(":price", itemsVector[i].price*100);
-                qry.bindValue(":quantity", itemsVector[i].quantity);
-                qry.bindValue(":category", itemsVector[i].category);
-                if(itemsVector[i].quantity > 0) qry.bindValue(":instock", true);
-                else qry.bindValue(":instock", false);
-                if(!qry.exec()){
-                    QMessageBox::warning(this, "Failed", "Update Failed to Execute ");
-                }
-                else
-                {
-                    QMessageBox::information(this, "Update", "Inventory updated");
-                }
-
-            }
-            else
-            {
-                QMessageBox::information(this, "Not Connected", "Database is not Connected");
-            }
-            db.closeDB();
-            break;
+            QMessageBox::information(this, "Update", "Inventory updated");
         }
+
     }
-
-    if(itemMatchFound != true)
+    else
     {
-        QMessageBox::information(this, "Update Item", "No matching Item ID found. Please try again.");
+        QMessageBox::information(this, "Not Connected", "Database is not Connected");
     }
+    db.closeDB();
 }
+
 
 
 void HomepageWindow::on_clearFieldsButtonInventory_clicked()
